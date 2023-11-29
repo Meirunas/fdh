8874da3c122437ffccdca5f14d54c1a7bdb57e3d
Meirunas Smitas <smeirunas@email.com>
2023-11-29 16:57:47 +0000

    fix: improve columns

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -21,13 +21,13 @@ export default function Zigzag() {
           <div className="grid gap-20">
 
             {/* 1st item */}
-            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+            <div className="md:grid md:grid-cols-2 md:gap-6 items-center">
               {/* Image */}
-              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto mb-8 md:mb-0 md:order-1" data-aos="fade-up">
                 <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage01} width={540} height={405} alt="Features 01" />
               </div>
               {/* Content */}
-              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto" data-aos="fade-right">
                 <div className="md:pr-4 lg:pr-12 xl:pr-16">
                   <div className="font-architects-daughter text-xl text-green-600 mb-2">Reach goals that matter</div>
                   <h3 className="h3 mb-3">Frontend – Beginners</h3>
@@ -69,13 +69,13 @@ export default function Zigzag() {
             </div>
 
             {/* 2nd item */}
-            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+            <div className="md:grid md:grid-cols-2 md:gap-6 items-center">
               {/* Image */}
-              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto mb-8 md:mb-0 rtl" data-aos="fade-up">
                 <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage02} width={540} height={405} alt="Features 02" />
               </div>
               {/* Content */}
-              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto" data-aos="fade-left">
                 <div className="md:pl-4 lg:pl-12 xl:pl-16">
                   <div className="font-architects-daughter text-xl text-green-600 mb-2">Reach goals that matter</div>
                   <h3 className="h3 mb-3">Frontend - Intermediate</h3>
